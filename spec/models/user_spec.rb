f64d417f1ec0bc1506bc82a9b6eb5baeffba3a6a
Raghu Betina <raghu@firstdraft.com>
2019-11-22 20:17:30 +0000

    2nd draft

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:flights) }
+
     end
 
     describe "InDirect Associations" do
@@ -11,6 +13,12 @@ RSpec.describe User, type: :model do
     end
 
     describe "Validations" do
+
+    it { should validate_uniqueness_of(:phone_number).with_message('Phone number is already associated with another account') }
+
+    it { should validate_presence_of(:phone_number) }
+
+    it { should validate_length_of(:phone_number).is_at_least(7).is_at_most(13) }
       
     end
 end
